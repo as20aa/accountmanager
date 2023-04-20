444d6c98d9d1c14e984ad97fbf6a824c9e9dcf00
as <1123554771@qq.com>
2023-04-20 10:24:35 +0800

    20230420 as add test application properties

--- src/main/java/com/as/accountmanager/util/IDGenerator.java
+++ src/main/java/com/as/accountmanager/util/IDGenerator.java
@@ -1,4 +1,11 @@
 package com.as.accountmanager.util;
 
+/**
+ * IDGenerator
+ * @date 20230419
+ * @author as
+ * @version 1
+ * @description use to generate user id for every new user
+ */
 public class IDGenerator {
 }
--- src/main/java/com/as/accountmanager/util/sequence/service/SequenceService.java
+++ src/main/java/com/as/accountmanager/util/sequence/service/SequenceService.java
@@ -8,22 +8,26 @@ import com.as.accountmanager.util.sequence.logic.SequenceLogic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+/***
+ * test service for sequence
+ */
+
 @RestController
 @RequestMapping(value="/sequence")
 public class SequenceService {
     @Autowired
     private SequenceLogic sequenceLogic;
 
-    @RequestMapping(method= RequestMethod.POST, value="/addSequence")
-    public ResponseVO addSequence(@RequestBody SequenceVO sequenceVO) {
-        sequenceLogic.addSequence(sequenceVO);
-        return Response.Ok();
-    }
-
     @RequestMapping(method=RequestMethod.POST, value="/getValue")
     public ResponseVO getValue(@RequestBody SequenceVO sequenceVO) {
         int value = sequenceLogic.getValue(sequenceVO.getName());
         Log.info(getClass(), "the sequence is: " + String.valueOf(value));
         return Response.Ok(String.valueOf(value));
+    }
+
+    @RequestMapping(method=RequestMethod.POST, value="/resetValue")
+    public ResponseVO resetSequence(@RequestBody SequenceVO sequenceVO) {
+        sequenceLogic.resetSequence(sequenceVO.getName());
+        return Response.Ok();
     }
 }
